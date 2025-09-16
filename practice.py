e62876496b7cf597ff2c43a112b0dfcbde895635
Ali Arsal <ali.arsal1512@gmail.com>
2025-09-16 13:57:51 -0400

    Tasks-Upload

--- practice.py
+++ practice.py
@@ -1,2 +1,38 @@
-a = "hello world!"
-print  (a.replace('o', 'r'))
+# a = "hello world !"
+
+# aa = a.split("o")
+# print (aa)
+
+import numpy as np
+# print (np.__version__)
+
+arr = np.arange(10)
+
+# print (arr)
+
+bool_arr = np.ones(10, dtype=bool)
+
+# print (bool_arr)
+
+selective_arr = arr[arr>4]
+
+# print (selective_arr)
+
+arr[arr>4]  = 2
+# print (arr)
+
+arr = np.arange(10)
+arr[arr%2 == 1] = -1
+# print(arr)
+
+arr = np.arange(10)
+arr = arr.reshape(5, -1)
+# print(arr)
+
+a = np.arange(10).reshape(2,-1)
+b = np.arange(0,20,2).reshape(2,-1)
+
+# print (np.vstack([a,b]))
+print(a)
+print(b)
+print(np.where(a == b))
